6871587a8a9e44a0eeacd20dd33c489564c52cab
ruwan9 <ruwan9@kakao.com>
2022-04-12 00:53:56 +0900

    20210412 기능 구현하기(UI 구성)

--- iOS/OddEvenGame/OddEvenGame/ViewController.swift
+++ iOS/OddEvenGame/OddEvenGame/ViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var computerBallCountLbl: UILabel!
+    @IBOutlet var userBallCountLbl: UILabel!
+    
+    var comBallsCount: Int = 20
+    var userBallsCount: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        computerBallCountLbl.text = String(comBallsCount)
+        userBallCountLbl.text = String(userBallsCount)
     }
+    
+    @IBAction func gameStartPressed(_ sender: Any) {
+        print("게임 시작!")
+    }
+    
 
 
 }
