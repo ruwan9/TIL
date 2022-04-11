b3ce8a7b9d9094cb43f2cc7d61290f44169fb337
ruwan9 <ruwan9@kakao.com>
2022-04-12 00:37:01 +0900

    First Commit

---
+++ iOS/OddEvenGame/OddEvenGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddEvenGame
+//
+//  Created by Wansoo Ryu on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
