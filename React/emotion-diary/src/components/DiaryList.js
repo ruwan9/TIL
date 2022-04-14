9789f5f3dff3871aac6462c47cbb3d2d701286df
ruwan9 <ruwan9@kakao.com>
2022-04-14 20:56:18 +0900

    20210414 홈 페이지 구현 - 감정 점수 filter

--- React/emotion-diary/src/components/DiaryList.js
+++ React/emotion-diary/src/components/DiaryList.js
@@ -5,8 +5,13 @@ const sortOptionList = [
   { value: "latest", name: "최신순" },
   { value: "oldest", name: "오래된 순" },
 ];
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
 
-// 정렬
+// 정렬 Component
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select name="" value={value} onChange={(e) => onChange(e.target.value)}>
@@ -20,7 +25,10 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 function DiaryList({ diaryList }) {
+  // 정렬 기준
   const [sortType, setSortType] = useState("latest");
+  // 감정 필터
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
     const compare = (a, b) => {
@@ -30,19 +38,34 @@ function DiaryList({ diaryList }) {
         return parseInt(a.date) - parseInt(b.date);
       }
     };
+    const filterCallback = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
     // Deep Copy
     const copyList = JSON.parse(JSON.stringify(diaryList));
 
-    const sortedList = copyList.sort(compare);
+    const filteredList = filter === "all" ? copyList : copyList.filter((it) => filterCallback(it));
+
+    const sortedList = filteredList.sort(compare);
 
     return sortedList;
   };
 
   return (
     <div>
+      {/* 정렬 기준 */}
       <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
+      {/* 감정 기준 */}
+      <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList} />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );
