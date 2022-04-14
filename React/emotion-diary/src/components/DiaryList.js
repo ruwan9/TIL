ac60cd325e314f482f9a4cc669bae9bd014ecafa
ruwan9 <ruwan9@kakao.com>
2022-04-14 13:48:13 +0900

    20210414 홈 페이지 구현 - 최신순 / 오래된순 일기 정렬 기능

--- React/emotion-diary/src/components/DiaryList.js
+++ React/emotion-diary/src/components/DiaryList.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+
+// 정렬 옵션
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+// 정렬
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select name="" value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 function DiaryList({ diaryList }) {
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    // Deep Copy
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+
+    const sortedList = copyList.sort(compare);
+
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => (
+      <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
