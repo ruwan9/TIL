92a09c1bf5d282c7326e12bc74c665096f1ff8bc
ruwan9 <ruwan9@kakao.com>
2022-04-15 20:41:16 +0900

    20210415 테스크 관리뷰, todolist 뷰모델 구현

--- iOS/TodoList/TodoList/TodoListViewController.swift
+++ iOS/TodoList/TodoList/TodoListViewController.swift
@@ -19,7 +19,7 @@ class TodoListViewController: UIViewController {
     
     
     // TODO: TodoViewModel 만들기
-    
+    let todoListViewModel = TodoViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +28,7 @@ class TodoListViewController: UIViewController {
         
         
         // TODO: 데이터 불러오기
-        
+        todoListViewModel.loadTasks()
     }
     
     @IBAction func isTodayButtonTapped(_ sender: Any) {
@@ -55,13 +55,17 @@ extension TodoListViewController {
 
 extension TodoListViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // TODO: 섹션 몇개 - 2개
-        return 2
+        // TODO: 섹션 몇개
+        return todoListViewModel.numOfSection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // TODO: 섹션별 아이템 몇개 - 5개
-        return 5
+        // TODO: 섹션별 아이템 몇개
+        if section == 0 {
+            return todoListViewModel.todayTodos.count
+        } else {
+            return todoListViewModel.upcompingTodos.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -69,8 +73,14 @@ extension TodoListViewController: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodoListCell", for: indexPath) as? TodoListCell else {
             return UICollectionViewCell()
         }
-        return cell
         
+        var todo: Todo
+        if indexPath.section == 0 {
+            todo = todoListViewModel.todayTodos[indexPath.item]
+        } else {
+            todo = todoListViewModel.upcompingTodos[indexPath.item]
+        }
+        cell.updateUI(todo: todo)
         // TODO: todo 를 이용해서 updateUI
         // TODO: doneButtonHandler 작성
         // TODO: deleteButtonHandler 작성
