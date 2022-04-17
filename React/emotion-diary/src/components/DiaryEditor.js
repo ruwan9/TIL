62717de46e1408b3b4a80584d6a04e4ffe3e777a
ruwan9 <ruwan9@kakao.com>
2022-04-17 20:52:21 +0900

    20220417 페이지 구현 - 일기 쓰기 + CSS

---
+++ React/emotion-diary/src/components/DiaryEditor.js
@@ -0,0 +1,116 @@
+import React, { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
+import EmotionItem from "./EmotionItem";
+import MyButton from "./MyButton";
+import MyHeader from "./MyHeader";
+
+const EmotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "끔찍함",
+  },
+];
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10); // ISO형식의 문자열을 반환 (0부터 9까지 -> yyyy/mm/dd)
+};
+
+function DiaryEditor() {
+  const navigate = useNavigate();
+  const [date, setDate] = useState(getStringDate(new Date()));
+  // DiaryDispatchContext로부터 onCreate 함수 받아오기
+  const { onCreate } = useContext(DiaryDispatchContext);
+  // 기본 감정 선택
+  const [emotion, setEmotion] = useState(3);
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+    console.log(date, content, emotion);
+    onCreate(date, content, emotion);
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={<MyButton text={"뒤로가기"} onClick={() => navigate(-1)} />}
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {EmotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="오늘은 어땠나요?"
+            ></textarea>
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
+            <MyButton text={"작성완료"} type={"positive"} onClick={handleSubmit} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default DiaryEditor;
---
+++ React/emotion-diary/src/components/EmotionItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function EmotionItem({ emotion_id, emotion_img, emotion_descript, onClick, isSelected }) {
+  return (
+    <div
+      className={[
+        "EmotionItem",
+        isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`,
+      ].join(" ")}
+      onClick={() => onClick(emotion_id)}
+    >
+      <img src={emotion_img} alt="Emotion" />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+}
+
+export default EmotionItem;
