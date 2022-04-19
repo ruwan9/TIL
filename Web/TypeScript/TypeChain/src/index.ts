5da9d6b178fe229ff4c946f1f48bb11dd71c3a58
ruwan9 <ruwan9@kakao.com>
2022-04-19 23:56:35 +0900

    20220419 typescript로 블록체인 만들기 - classes

--- Web/TypeScript/TypeChain/src/index.ts
+++ Web/TypeScript/TypeChain/src/index.ts
@@ -1,19 +1,32 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+// const person = {
+//   name: "bob",
+//   age: 24,
+//   gender: "male",
+// };
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "bob",
-  age: 24,
-  gender: "male",
-};
+const bob = new Human("bob", 24, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, aged ${person.age}! You are a ${person.gender}.`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(bob));
 
 export {};
