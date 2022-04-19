7a380f5121a4a78450aa6f2fa193f8e436103c44
ruwan9 <ruwan9@kakao.com>
2022-04-19 23:51:30 +0900

    20220419 typescript로 블록체인 만들기 - interface

--- Web/TypeScript/TypeChain/src/index.ts
+++ Web/TypeScript/TypeChain/src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, aged ${age}! You are a ${gender}.`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "bob",
+  age: 24,
+  gender: "male",
 };
 
-console.log(sayHi("Taylor", 24, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, aged ${person.age}! You are a ${person.gender}.`;
+};
+
+console.log(sayHi(person));
 
 export {};
