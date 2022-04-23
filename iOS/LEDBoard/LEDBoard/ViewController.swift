3be82a19a9e17e500390fbbb4638d2c2793df90c
ruwan9 <ruwan9@kakao.com>
2022-04-24 01:52:54 +0900

    220220424 UI 초기 세팅

---
+++ iOS/LEDBoard/LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by Wansoo Ryu on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
