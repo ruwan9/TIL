eca0df6b6b1af9e1c4cb45a277bf9cce26317a44
ruwan9 <ruwan9@kakao.com>
2022-04-26 02:32:39 +0900

    20220426 계산기 기능 구현하기-1

--- iOS/Calculator/Calculator/ViewController.swift
+++ iOS/Calculator/Calculator/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Substract
+    case Divide
+    case Multiply
+    case unknown
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    @IBOutlet weak var acButton: UIButton!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation: Operation = .unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+//         // .title(for: .normal)을 사용하면 nil값 반환... 왜인지는 모르겠...
+//        guard let numberValue = sender.title(for: .normal) else {return}
+        guard let numberValue = sender.titleLabel?.text else { return }
+               if self.displayNumber.count < 9 {
+                   self.displayNumber += numberValue
+                   self.numberOutputLabel.text = self.displayNumber
+               }
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOperation = .unknown
+        self.numberOutputLabel.text = "0"
+    }
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+                    self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+                    self.numberOutputLabel.text = self.displayNumber
+                }
+    }
+    @IBAction func tapDivideButton(_ sender: Any) {
+    }
+    @IBAction func tapMultiplyButton(_ sender: Any) {
+    }
+    @IBAction func tapSubtractButton(_ sender: Any) {
+    }
+    @IBAction func tapAddButton(_ sender: Any) {
+    }
+    @IBAction func tapEqualButton(_ sender: Any) {
+    }
+    
 }
 
