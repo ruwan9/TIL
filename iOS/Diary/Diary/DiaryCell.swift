c21d35dd5c101079b6342fe3f25bf7f288174a2b
ruwan9 <ruwan9@kakao.com>
2022-04-26 22:00:13 +0900

    20220426 기능 구현 - 날짜 DatePicker 구현

--- iOS/Diary/Diary/DiaryCell.swift
+++ iOS/Diary/Diary/DiaryCell.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 class DiaryCell: UICollectionViewCell {
-    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
 }
--- iOS/Diary/Diary/WriteDiaryViewController.swift
+++ iOS/Diary/Diary/WriteDiaryViewController.swift
@@ -9,21 +9,47 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.configureContentTextView()
+        self.configureDatePicker()
     }
     
+    private func configureContentTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.contentTextView.layer.borderColor = borderColor.cgColor
+        self.contentTextView.layer.borderWidth = 0.5
+        self.contentTextView.layer.cornerRadius = 5.0
     }
-    */
-
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datepickerValueDidChange(_:)), for: .valueChanged)
+        self.dateTextField.inputView = self.datePicker
+    }
+    @objc private func datepickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formatter.string(from: datePicker.date)
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
