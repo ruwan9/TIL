a3442f5edb5e8a66432f903970cf12ee5b9cf8f7
ruwan9 <ruwan9@kakao.com>
2022-04-26 10:56:43 +0900

    20220426 계산기 기능 구현하기 마무리

--- iOS/Calculator/Calculator/ViewController.swift
+++ iOS/Calculator/Calculator/ViewController.swift
@@ -55,14 +55,58 @@ class ViewController: UIViewController {
                 }
     }
     @IBAction func tapDivideButton(_ sender: Any) {
+        self.operation(.Divide)
     }
     @IBAction func tapMultiplyButton(_ sender: Any) {
+        self.operation(.Multiply)
     }
     @IBAction func tapSubtractButton(_ sender: Any) {
+        self.operation(.Substract)
     }
     @IBAction func tapAddButton(_ sender: Any) {
+        self.operation(.Add)
     }
     @IBAction func tapEqualButton(_ sender: Any) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else {return}
+                guard let secondOperand = Double(self.secondOperand) else {return}
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Substract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                default:
+                    break
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            self.currentOperation = operation
+            
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
+        
     }
     
 }
