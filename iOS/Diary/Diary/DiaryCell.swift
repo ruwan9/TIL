5f9b4fc0606e2761cb6f800baac63071e528425c
ruwan9 <ruwan9@kakao.com>
2022-04-26 17:58:10 +0900

    20220426 프로젝트 초기 세팅 - UI 그리기

---
+++ iOS/Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by Wansoo Ryu on 2022/04/26.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ iOS/Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by Wansoo Ryu on 2022/04/26.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
