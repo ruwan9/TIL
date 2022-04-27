d82627b2d281d1da64f30bbb4e2a93e56d71fe90
ruwan9 <ruwan9@kakao.com>
2022-04-27 21:12:35 +0900

    20220427 챗봇 구현

---
+++ React/chatbot/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Chatbot from "./Chatbot";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChatBot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
