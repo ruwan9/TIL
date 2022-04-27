e748a329c5da7e255fa1f082b6987edb9ec2e0fc
ruwan9 <ruwan9@kakao.com>
2022-04-27 21:35:06 +0900

    20220427 프로젝트 기본 세팅 / UI 구성

---
+++ iOS/Pomodoro/Pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Wansoo Ryu on 2022/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
