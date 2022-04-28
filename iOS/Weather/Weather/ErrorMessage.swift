434f384fdf4b468ab5619c74c9aac02591f435f0
ruwan9 <ruwan9@kakao.com>
2022-04-28 23:04:54 +0900

    20220428 기능 구현하기 - 날씨 보여주기 / 에러 메시지 구현

---
+++ iOS/Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by Wansoo Ryu on 2022/04/28.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable {
+    let message: String
+}
--- iOS/Weather/Weather/ViewController.swift
+++ iOS/Weather/Weather/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
-    @IBOutlet weak var tempLabel: UIButton!
+    @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +33,40 @@ class ViewController: UIViewController {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=cc4e0e49a3c8ca49d208746f8f5ac500") else {return}
         
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) {data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
+            let successRange = (200..<300)
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return}
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else {return}
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
+            }
 
         }.resume()
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather =  weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))℃"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))℃"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))℃"
     }
     
 }
