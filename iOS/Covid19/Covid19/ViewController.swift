a8f60c6574de2bc3ea4968803356c361e385ec09
ruwan9 <ruwan9@kakao.com>
2022-04-29 15:52:56 +0900

    20220429 기능 구현 - API 연결

--- iOS/Covid19/Covid19/ViewController.swift
+++ iOS/Covid19/Covid19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -16,9 +18,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.fetchCovidOverview(completionHandler: {[weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case let .success(result):
+                print("Success \(result)")
+            case let .failure(error):
+                print("Failure \(error)")
+                
+            }
+        } )
     }
 
+    func fetchCovidOverview(completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "fUbvFmca6IAVTj9KyiQlEeR2PNZWMSk4x"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+        }
+        
+    }
 
 }
 
