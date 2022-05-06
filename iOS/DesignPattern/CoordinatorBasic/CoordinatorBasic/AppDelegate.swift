90c2365eb0b756c2e205083ce34e813ebc1d870f
ruwan9 <ruwan9@kakao.com>
2022-05-06 22:49:41 +0900

    20220506 Practice 초기 세팅

---
+++ iOS/DesignPattern/CoordinatorBasic/CoordinatorBasic/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CoordinatorBasic
+//
+//  Created by Wansoo Ryu on 2022/05/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ iOS/DesignPattern/CoordinatorBasic/CoordinatorBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorBasic
+//
+//  Created by Wansoo Ryu on 2022/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
