a8c1bf9d7c043988ccffbf65dc1e434a403b1eaf
ruwan9 <ruwan9@kakao.com>
2022-05-12 22:47:02 +0900

    20220512 recoil 복습

--- Front/React/recoil-tutorial/src/ReadWriteCount.js
+++ Front/React/recoil-tutorial/src/ReadWriteCount.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { useRecoilState, useSetRecoilState, useResetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState, useResetRecoilState, useRecoilValue } from "recoil";
 import { countState } from "./recoil/count";
 
 // 읽기 및 쓰기 컴포넌트
 function ReadWriteCount() {
   const [count, setCount] = useRecoilState(countState); // useRecoilState 을 통한 value, setter 반환
+  const getCount = useRecoilValue(countState);
   const setCountUseSetRecoilState = useSetRecoilState(countState); // 값을 변경하는 함수만 반환
   const resetCount = useResetRecoilState(countState); // 설정된 기본값으로 리셋
 
+  console.log(getCount);
   return (
     <div>
       <h2>읽기 쓰기 카운트 컴포넌트</h2>
