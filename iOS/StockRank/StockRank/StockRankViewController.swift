f842ab87490b26815532e8950c40639ed5d2624a
ruwan9 <ruwan9@kakao.com>
2022-05-21 19:44:10 +0900

    20220521 collectionView autolayout 완료

---
+++ iOS/StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by Wansoo Ryu on 2022/05/21.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
