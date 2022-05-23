993bffa9a834447ab7e76ad0cfb5ed3f517adab9
ruwan9 <ruwan9@kakao.com>
2022-05-23 21:14:56 +0900

    20220523 datasource, delegate 구현

---
+++ iOS/StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by Wansoo Ryu on 2022/05/23.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
--- iOS/StockRank/StockRank/StockRankViewController.swift
+++ iOS/StockRank/StockRank/StockRankViewController.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class StockRankViewController: UIViewController {
+    
+    let stockList: [StockModel] = StockModel.list
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+      
     }
     
 
+}
+
+extension StockRankViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
+        return cell
+//        return UICollectionViewCell()
+    }
+}
+
+extension StockRankViewController: UICollectionViewDelegate {
+    
 }
