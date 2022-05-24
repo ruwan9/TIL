0ae8f0504e95ea8a730db0bc6ff376017edbd15c
ruwan9 <ruwan9@kakao.com>
2022-05-24 16:27:03 +0900

    20220524 음수 색 변환 과제 추가

--- iOS/StockRank/StockRank/StockRankCollectionViewCell.swift
+++ iOS/StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -20,6 +20,22 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         companyIconImageView.image = UIImage(named: stock.imageName)
         companyNameLabel.text = "\(stock.name)"
         companyPriceLabel.text = "\(convertToCurrencyFormat(price: stock.price)) 원"
+        
+        // 과제 방법 1 - 함수 만들기
+//        diffLabel.text = "\(configureDiffColor(diff: stock.diff))%"
+
+        // 과제 방법 2 - if/else
+//        let color: UIColor
+//        if stock.diff < 0 {
+//            color = .systemBlue
+//        } else {
+//            color = .systemRed
+//        }
+//        diffLabel.textColor = color
+//        diffLabel.text = "\(stock.diff)%"
+        
+        // 과제 방법 3 - 삼항연산자
+        diffLabel.textColor = stock.diff < 0 ? UIColor.systemBlue : UIColor.systemRed
         diffLabel.text = "\(stock.diff)%"
     }
     
@@ -30,5 +46,15 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         numberFormatter.maximumFractionDigits = 0
         let result = numberFormatter.string(from: NSNumber(value: price)) ?? ""
         return result
+    }
+    
+    // 과제 방법 1 - 음수 diff 색깔 변환
+    func configureDiffColor(diff: Double) -> Double {
+        if diff < 0 {
+            diffLabel.textColor = UIColor.blue
+        } else {
+            diffLabel.textColor = UIColor.red
+        }
+        return diff
     }
 }
