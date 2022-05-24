78b2aaae02c2eebf4ae2e47a6d6236bb329e00dc
ruwan9 <ruwan9@kakao.com>
2022-05-25 01:02:06 +0900

    20220525 프로젝트 초기 세팅 - autolayout 적용

---
+++ iOS/ChatList/ChatList/ChatListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by Wansoo Ryu on 2022/05/25.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
