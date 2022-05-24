432cfdfa06eace40bb167b5474e6d040bb3887f0
ruwan9 <ruwan9@kakao.com>
2022-05-24 16:09:41 +0900

    20220524 리스트 추가, delegate 추가

--- iOS/StockRank/StockRank/StockRankCollectionViewCell.swift
+++ iOS/StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class StockRankCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var companyIconImageView: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyPriceLabel: UILabel!
+    @IBOutlet weak var diffLabel: UILabel!
+    
+    func configure(_ stock: StockModel) {
+        rankLabel.text = "\(stock.rank)"
+        companyIconImageView.image = UIImage(named: stock.imageName)
+        companyNameLabel.text = "\(stock.name)"
+        companyPriceLabel.text = "\(stock.price) 원"
+        diffLabel.text = "\(stock.diff)%"
+    }
 }
--- iOS/StockRank/StockRank/StockRankViewController.swift
+++ iOS/StockRank/StockRank/StockRankViewController.swift
@@ -31,12 +31,20 @@ extension StockRankViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath) as? StockRankCollectionViewCell else {return UICollectionViewCell()}
+        
+        let stock = stockList[indexPath.item]
+        cell.configure(stock)
+        
         return cell
-//        return UICollectionViewCell()
     }
 }
 
-extension StockRankViewController: UICollectionViewDelegate {
-    
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // width == collectionView
+        // height ==  80
+        
+        return CGSize(width: collectionView.bounds.width, height: 80)
+    }
 }
