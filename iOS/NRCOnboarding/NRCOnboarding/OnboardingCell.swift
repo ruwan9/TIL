2e0e457b2fb0dd684b5e3588e82505d776c2ecec
ruwan9 <ruwan9@kakao.com>
2022-06-09 16:55:41 +0900

    20220609 collectionview 추가

---
+++ iOS/NRCOnboarding/NRCOnboarding/OnboardingCell.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingCell.swift
+//  NRCOnboarding
+//
+//  Created by Wansoo Ryu on 2022/06/09.
+//
+
+import UIKit
+
+class OnboardingCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ message: OnboardingMessage) {
+        thumbnailImageView.image = UIImage(named: message.imageName)
+        titleLabel.text = message.title
+        descriptionLabel.text = message.description
+    }
+}
--- iOS/NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
+++ iOS/NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    let messages: [OnboardingMessage] = OnboardingMessage.messages
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout { layout.estimatedItemSize = .zero}
     }
+    
+}
+
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {return UICollectionViewCell()}
+        
+        let message = messages[indexPath.item]
+        cell.configure(message)
+        return cell
+    }
+    
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
     
 }
