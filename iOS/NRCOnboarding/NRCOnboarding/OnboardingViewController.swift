a9447e22e1b2f3eac9956d506e842af0a7d5cb14
ruwan9 <ruwan9@kakao.com>
2022-06-09 16:20:38 +0900

    20220609 페이징뷰 기본 세팅

---
+++ iOS/NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboarding
+//
+//  Created by Wansoo Ryu on 2022/06/09.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
