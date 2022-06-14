553d24fc5bf0c733c2b3b22e714f2561303d5d3d
ruwan9 <ruwan9@kakao.com>
2022-06-14 20:31:54 +0900

    20220614 diffableDataSource 초기 세팅

---
+++ iOS/AppleFrameworkV2/AppleFramework/FrameworkListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FrameworkListViewController.swift
+//  AppleFramework
+//
+//  Created by joonwon lee on 2022/04/24.
+//
+
+import UIKit
+
+class FrameworkListViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [AppleFramework] = AppleFramework.list
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = AppleFramework
+    enum Section {
+        case main
+        
+    }
+    // Data, Presentation, Layout
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        navigationController?.navigationBar.topItem?.title = "☀️ Apple Frameworks"
+    }
+}
+
+extension FrameworkListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let framework = list[indexPath.item]
+        print(">>> selected: \(framework.name)")
+    }
+}
